1a298df593e6219882c5248e017df5fc2f51efd6
Phelipe Teles <phelipe_teles@hotmail.com>
2020-11-11 09:59:42 -0300

    Add basic routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./App.css";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import { Navigation } from "./components/Navigation";
+import { Search } from "./components/Search";
+import { Playlists } from "./components/Playlists";
+import { HomePage } from "./components/HomePage";
+
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    flexFlow: "column"
+  }
+}));
+
 function App() {
-  return <div className="App"></div>;
+  const classes = useStyles();
+
+  return (
+    <Router>
+      <div className={classes.root}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/playlists" component={Playlists} />
+        </Switch>
+        <Navigation />
+      </div>
+    </Router>
+  );
 }
 
 export default App;

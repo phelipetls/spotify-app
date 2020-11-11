4926ea3bfb4e1efd42780d1d0087d5fe530ff12f
Phelipe Teles <phelipe_teles@hotmail.com>
2020-11-11 18:07:54 -0300

    Add LoginPage component

---
+++ src/components/LoginPage.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { Grid, Typography, Link, Button } from "@material-ui/core";
+
+import { getAuthUrl } from "../utils/spotify-auth";
+
+export function LoginPage() {
+  const authUrl = getAuthUrl();
+
+  return (
+    <Grid container alignItems="center" justify="center" direction="column">
+      <div>
+        <Typography align="center" variant="h4">
+          Spotify playlists
+        </Typography>
+
+        <Typography align="center" paragraph>
+          Clique no link abaixo para fazer login
+        </Typography>
+      </div>
+
+      <Link href={authUrl}>
+        <Button variant="contained" color="primary">
+          Login
+        </Button>
+      </Link>
+    </Grid>
+  );
+}
+
+export default LoginPage;

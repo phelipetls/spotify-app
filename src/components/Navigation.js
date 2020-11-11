f0c32bdd17f9be43c29140e3c7c79e2ee88da923
Phelipe Teles <phelipe_teles@hotmail.com>
2020-11-11 09:59:02 -0300

    Add Navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
+import { Home, QueueMusic, Search } from "@material-ui/icons";
+
+import { Link as RouterLink } from "react-router-dom";
+
+export function Navigation() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (_, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <BottomNavigation value={value} onChange={handleChange} showLabels>
+      <BottomNavigationAction
+        component={RouterLink}
+        to="/"
+        label="Home"
+        icon={<Home />}
+      />
+      <BottomNavigationAction
+        component={RouterLink}
+        to="/search"
+        label="Search"
+        icon={<Search />}
+      />
+      <BottomNavigationAction
+        component={RouterLink}
+        to="/playlists"
+        label="Playlists"
+        icon={<QueueMusic />}
+      />
+    </BottomNavigation>
+  );
+}

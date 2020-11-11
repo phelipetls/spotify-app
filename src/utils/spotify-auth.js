ff750f21108f78cc6113d721b0d9465606ca379a
Phelipe Teles <phelipe_teles@hotmail.com>
2020-11-11 18:08:08 -0300

    Add function to get Spotify URL for authentication
    Based on the documentation at
    https://developer.spotify.com/documentation/general/guides/authorization-guide/#implicit-grant-flow.
    
    This method (Implicit grant flow) allow us to get a temporary key (2
    hours) without using any secret keys.

---
+++ src/utils/spotify-auth.js
@@ -0,0 +1,33 @@
+const BASE_URL = "https://accounts.spotify.com";
+const AUTH_ENDPOINT = "authorize";
+const CLIENT_ID = "35d25e5a01eb4c87a4458be1b0786bb8";
+const RESPONSE_TYPE = "token";
+const REDIRECT_URI = "http://localhost:3000/";
+const SCOPE = [
+  "playlist-modify-public",
+  "playlist-modify-private",
+  "playlist-read-private",
+  "playlist-read-collaborative"
+];
+
+const getRandomString = length => {
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  return Array.from({ length })
+    .map(() => possible[Math.floor(Math.random() * possible.length)])
+    .join("");
+};
+
+export function getAuthUrl() {
+  const queryString = new URLSearchParams();
+
+  queryString.set("client_id", CLIENT_ID);
+  queryString.set("response_type", RESPONSE_TYPE);
+  queryString.set("response_type", RESPONSE_TYPE);
+  queryString.set("redirect_uri", new URL(REDIRECT_URI));
+  queryString.set("scope", SCOPE);
+  queryString.set("state", getRandomString(16));
+
+  return new URL(AUTH_ENDPOINT + "?" + queryString, BASE_URL);
+}

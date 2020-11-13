95697d2c147613df36e60f3825d99c42e2abba71
Phelipe Teles <phelipe_teles@hotmail.com>
2020-11-12 22:58:22 -0300

    Add Material-UI custom theme to match with Spotify palette

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
 import { BrowserRouter as Router } from "react-router-dom";
+
+import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { theme } from "./styles/Theme.js";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
---
+++ src/styles/Theme.js
@@ -0,0 +1,40 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ptBR } from "@material-ui/core/locale";
+
+export const theme = createMuiTheme(
+  {
+    palette: {
+      primary: {
+        main: "#1DB954",
+        contrastText: "#FFF"
+      },
+      secondary: {
+        main: "#171717",
+        contrastText: "#FFF"
+      },
+      text: {
+        primary: "#FFF",
+        secondary: "#B3B3B3"
+      },
+      background: {
+        default: "#121212",
+        paper: "#171717"
+      },
+      action: {
+        active: "#B3B3B3",
+        selected: "#282828",
+        hover: "#232323",
+      },
+      divider: "rgb(128, 128, 128)"
+    },
+    typography: {
+      subtitle1: {
+        fontSize: "smaller"
+      },
+      subtitle2: {
+        fontSize: "x-smaller"
+      }
+    }
+  },
+  ptBR
+);

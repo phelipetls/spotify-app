b481be4cfadb9db352bd6301aa890c04ac022867
Phelipe Teles <phelipe_teles@hotmail.com>
2020-11-15 17:50:10 -0300

    Reduce scope, as we do not need to read/mutate playlists

--- src/utils/spotify-auth-url.js
+++ src/utils/spotify-auth-url.js
@@ -5,15 +5,7 @@ const AUTH_ENDPOINT = "authorize";
 const CLIENT_ID = "35d25e5a01eb4c87a4458be1b0786bb8";
 const RESPONSE_TYPE = "token";
 const REDIRECT_URI = "http://localhost:3000/auth";
-const SCOPE = [
-  "user-read-private",
-  "user-top-read",
-  "user-library-read",
-  "playlist-modify-public",
-  "playlist-modify-private",
-  "playlist-read-private",
-  "playlist-read-collaborative"
-];
+const SCOPE = ["user-read-private", "user-top-read", "user-library-read"];
 
 export function getAuthUrl() {
   const queryString = new URLSearchParams();

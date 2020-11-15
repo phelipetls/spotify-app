fb4c9f5e3cd17f95ce3c6c98d99e2ecc9e4628a5
Phelipe Teles <phelipe_teles@hotmail.com>
2020-11-15 19:56:07 -0300

    Modify spotify redirect uri for authentication on production

--- src/utils/spotify-auth-url.js
+++ src/utils/spotify-auth-url.js
@@ -4,7 +4,10 @@ const BASE_URL = "https://accounts.spotify.com";
 const AUTH_ENDPOINT = "authorize";
 const CLIENT_ID = "35d25e5a01eb4c87a4458be1b0786bb8";
 const RESPONSE_TYPE = "token";
-const REDIRECT_URI = "http://localhost:3000/auth";
+const REDIRECT_URI =
+  process.env.NODE_ENV === "production"
+    ? "https://my-playlists.netlify.app/auth"
+    : "http://localhost:3000/auth";
 const SCOPE = ["user-read-private", "user-top-read", "user-library-read"];
 
 export function getAuthUrl() {

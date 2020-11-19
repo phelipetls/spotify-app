9d3be04075e168a5c40694c807b3c92e7d618615
Phelipe Teles <phelipe_teles@hotmail.com>
2020-11-19 17:33:07 -0300

    Add setup to mock network requests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+// Configuring nock to work with axios
+// see https://www.npmjs.com/package/nock#axios
+import axios from "axios";
+import nock from "nock";
+import http from "axios/lib/adapters/http";
+
+axios.defaults.adapter = http;
--- src/utils/test-utils.js
+++ src/utils/test-utils.js
@@ -6,7 +6,19 @@ import { PlaylistsProvider } from "../context/playlists";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { theme } from "../styles/Theme";
 
-import { MemoryRouter as Router } from "react-router-dom";
+import { MemoryRouter as Router, useLocation } from "react-router-dom";
+
+const LocationDisplay = () => {
+  const location = useLocation();
+
+  return <div data-testid="location-display">{location.pathname}</div>;
+};
+
+const TokenDisplay = () => {
+  const { token } = useAuth();
+
+  return <div data-testid="token-display">{token}</div>;
+};
 
 const customRender = (
   component,
